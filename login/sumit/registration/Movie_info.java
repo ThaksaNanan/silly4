950bca808214ff45f3a700116b6a3491361785a9
ThaksaNanan <48822516+ThaksaNanan@users.noreply.github.com>
2019-05-11 16:45:36 +0700

    Add files via upload

---
+++ login/sumit/registration/Movie_info.java
@@ -0,0 +1,25 @@
+package login.sumit.registration;
+
+public class Movie_info {
+	private String Name;
+	private String Time;
+	private String Language;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getTime() {
+		return Time;
+	}
+	public void setTime(String time) {
+		Time = time;
+	}
+	public String getLanguage() {
+		return Language;
+	}
+	public void setLanguage(String language) {
+		Language = language;
+	}
+}
---
+++ login/sumit/registration/Myprovider.java
@@ -0,0 +1,9 @@
+package login.sumit.registration;
+
+public interface Myprovider {
+	
+	String username = "root";
+	String pwd = "";
+	String connUrl = "jdbc:mysql://localhost/Silly4";//postgresql==>mysql
+
+}
